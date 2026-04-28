ac03a1645dddfbd684254e69e6c6a74e0d2c14f9
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2026-04-28 11:15:59 +0300

    feat: update project tt_um_bgr from guru-gitrepo/TTSKY26a-analog-bgr
    Commit: 6694082e776da1044d19a9142ef843eba3841f0c
    Workflow: https://github.com/guru-gitrepo/TTSKY26a-analog-bgr/actions/runs/25041671058

---
+++ projects/tt_um_bgr/tt_um_bgr.v
@@ -0,0 +1,27 @@
+/* Copyright (c) 2024 Guruprasad
+ * SPDX-License-Identifier: Apache-2.0
+ */
+module tt_um_bgr (
+    input  [7:0] ui_in,
+    output [7:0] uo_out,
+    inout  [7:0] uio,
+    input        clk,
+    input        rst_n,
+    output [7:0] ua,
+    input        VGND,
+    input        VDPWR
+);
+
+
+    // --- Analog placeholder ---
+    // Since CI is digital-only, we just drive a dummy value
+
+    assign ua[7:0] = 8'b0;
+
+    // No digital outputs
+    assign uo_out = 8'b0;
+
+    // No bidirectional usage
+    assign uio = 8'bz;
+
+endmodule
